09accde1b3be69264b48783b7a369567a0272037
mybaseball52 <mybaseball52@gmail.com>
2017-07-18 08:57:17 +0800

    HTTP Calls

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users',function(req,res){
+    res.json([
+        {"id": 1, "firstName": "Kevin", "lastName": "Huang", "email": "mybaseball52@gmail.com"},
+        {"id": 2, "firstName": "Ken", "lastName": "Huang", "email": "yourbaseball52@gmail.com"},
+        {"id": 3, "firstName": "Kevin", "lastName": "Hung", "email": "hisbaseball52@gmail.com"}
+    ]);
+})
+
 app.listen(port, (err) =>{
     if(err)
         console.log(err);

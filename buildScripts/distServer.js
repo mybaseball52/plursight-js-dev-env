4f92babaab3c9496e7ac7480bb69b5b119fe45ed
mybaseball52 <mybaseball52@gmail.com>
2017-07-20 09:14:09 +0800

    prod deploy

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -15,15 +15,6 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-//Hard Code
-app.get('/users',function(req,res){
-    res.json([
-        {"id": 1, "firstName": "Kevin", "lastName": "Huang", "email": "mybaseball52@gmail.com"},
-        {"id": 2, "firstName": "Ken", "lastName": "Huang", "email": "yourbaseball52@gmail.com"},
-        {"id": 3, "firstName": "Kevin", "lastName": "Hung", "email": "hisbaseball52@gmail.com"}
-    ]);
-})
-
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,7 @@
 export default function getBaseUrl() {
   // const inDevelopment = window.location.hostname === 'localhost';
   // return inDevelopment ?  'http://localhost:3001/' : '/';
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://nameless-beyond-18830.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
